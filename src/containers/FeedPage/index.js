72de4d04d97c5c5a7c31d81db89cd4d2ca3d1217
Kelson <kelsoncarmozine@globo.com>
2019-11-12 11:38:28 -0300

    ProtectRoutes

---
+++ src/containers/FeedPage/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react"
+import { connect } from "react-redux"
+
+
+
+
+const FeedPage = () => {
+    
+    return (
+        <div>aaa</div>
+    );
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+    }
+}
+
+export default connect(null, mapDispatchToProps)(FeedPage);
---
+++ src/containers/PostPage/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react"
+import { connect } from "react-redux"
+
+
+
+
+const PostPage = () => {
+
+    return (
+        <div>b</div>
+    );
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+    }
+}
+
+export default connect(null, mapDispatchToProps)(PostPage);
